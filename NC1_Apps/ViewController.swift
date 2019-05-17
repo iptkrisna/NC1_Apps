7f0323d7ca538ffee75fefb735c8bac1664a42fe
kulitbitu <krisnamath9@gmail.com>
2019-05-17 13:12:48 +0700

    Initial Commit

---
+++ NC1_Apps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NC1_Apps
+//
+//  Created by I Putu Krisna on 17/05/19.
+//  Copyright © 2019 I Putu Krisna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
